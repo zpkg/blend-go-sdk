ab5e4f6ceb68b9931635ea0a63ea68495ec82310
Will Charczuk <will@blendlabs.com>
2018-04-27 12:30:10 -0700

    adding extra fields

--- oauth/result.go
+++ oauth/result.go
@@ -1,8 +1,15 @@
 package oauth
 
+import "time"
+
 // Result is the final result of the oauth exchange.
 // It is the user profile of the user and the state information.
 type Result struct {
+	AccessToken  string
+	TokenType    string
+	RefreshToken string
+	Expiry       time.Time
+
 	Profile *Profile
 	State   *State
 }
