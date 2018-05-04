6b84d16a2d01bb9d7b6483f4d08fcf20d1982163
Will Charczuk <will@blendlabs.com>
2018-05-03 18:47:38 -0700

    yuno

--- raft/_example/node/main.go
+++ raft/_example/node/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/blend/go-sdk/logger"
 	"github.com/blend/go-sdk/raft"
 )
 
-func main() {
-	log := logger.All().WithDisabled(logger.Error)
+var (
+	id       = flag.String("id", "", "The raft worker id")
+	selfAddr = flag.String("self-addr", "", "Our address in the peer list")
+	bindAddr = flag.String("bind-addr", "", "Bind address")
+)
 
-	cfg := raft.NewConfigFromEnv()
+func main() {
+	flag.Parse()
+	log := logger.All()
+
+	cfg := raft.NewConfigFromEnv().WithID(*id).WithSelfAddr(*selfAddr).WithBindAddr(*bindAddr)
+
 	r := raft.NewFromConfig(cfg).WithLogger(log)
+	log.WithHeading(r.ID())
 	for _, remoteAddr := range cfg.GetPeers() {
 		if !r.IsSelf(remoteAddr) {
 			log.SyncDebugf("adding peer %s", remoteAddr)
