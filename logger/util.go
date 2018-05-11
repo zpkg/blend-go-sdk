367e2b1bbfe44851e710913c0aa5935aa3e29004
Will Charczuk <will@blendlabs.com>
2018-05-11 12:42:32 -0700

    logger tweaks

--- logger/util.go
+++ logger/util.go
@@ -43,3 +43,8 @@ func GetIP(r *http.Request) string {
 	ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 	return ip
 }
+
+// GetUserAgent gets a user agent from a request.
+func GetUserAgent(r *http.Request) string {
+	return r.UserAgent()
+}
