d8617590731855cd427f700f722917fc98b82619
Will Charczuk <will@blendlabs.com>
2018-05-14 20:47:39 -0700

    updates

--- secrets/_examples/client/main.go
+++ secrets/_examples/client/main.go
@@ -9,29 +9,29 @@ func main() {
 	log := logger.All()
 	client := secrets.Must(secrets.NewFromEnv()).WithLogger(log)
 
-	if err := client.Put("secret/data/foo/bar", secrets.Values{"value": "THE FOOOS"}); err != nil {
+	if err := client.Put("foo/bar", secrets.Values{"value": "THE FOOOS"}); err != nil {
 		log.SyncFatalExit(err)
 	}
-	if err := client.Put("secret/data/foo/baz", secrets.Values{"value": "THE BUZZ"}); err != nil {
+	if err := client.Put("foo/baz", secrets.Values{"value": "THE BUZZ"}); err != nil {
 		log.SyncFatalExit(err)
 	}
 
-	values, err := client.Get("secret/data/foo/bar")
+	values, err := client.Get("foo/bar")
 	if err != nil {
 		log.SyncFatalExit(err)
 	}
 	log.Infof("values: %#v", values)
 
-	values, err = client.Get("secret/data/foo/baz")
+	values, err = client.Get("foo/baz")
 	if err != nil {
 		log.SyncFatalExit(err)
 	}
 	log.Infof("values: %#v", values)
 
-	if err := client.Delete("secret/data/foo/baz"); err != nil {
+	if err := client.Delete("foo/baz"); err != nil {
 		log.SyncFatalExit(err)
 	}
-	if err := client.Delete("secret/data/foo/bar"); err != nil {
+	if err := client.Delete("foo/bar"); err != nil {
 		log.SyncFatalExit(err)
 	}
 	log.Infof("~fin~")
