05d5fc696cbf9647f3569022204808f55a7fb808
Will Charczuk <will@blendlabs.com>
2018-05-15 12:17:55 -0700

    tests

--- secrets/client_test.go
+++ secrets/client_test.go
@@ -9,6 +9,22 @@ import (
 	"github.com/blend/go-sdk/assert"
 )
 
+func TestClientGetVersion(t *testing.T) {
+	assert := assert.New(t)
+
+	client, err := New()
+	assert.Nil(err)
+
+	mountMetaJSON := `{"request_id":"e114c628-6493-28ed-0975-418a75c7976f","lease_id":"","renewable":false,"lease_duration":0,"data":{"accessor":"kv_45f6a162","config":{"default_lease_ttl":0,"force_no_cache":false,"max_lease_ttl":0,"plugin_name":""},"description":"key/value secret storage","local":false,"options":{"version":"2"},"path":"secret/","seal_wrap":false,"type":"kv"},"wrap_info":null,"warnings":null,"auth":null}`
+
+	m := NewMockHTTPClient().WithString("GET", URL("%s/v1/sys/internal/ui/mounts/secret/", client.Remote().String()), mountMetaJSON)
+	client.WithHTTPClient(m)
+
+	version, err := client.getVersion()
+	assert.Nil(err)
+	assert.Equal(Version2, version)
+}
+
 func TestClientGetMountMeta(t *testing.T) {
 	assert := assert.New(t)
 
