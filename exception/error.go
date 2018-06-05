78323c5477de2da834a704dcd2a0db0f147ef97e
Will Charczuk <will@blendlabs.com>
2018-06-05 13:38:21 -0700

    adding marshal json for error

--- exception/error.go
+++ exception/error.go
@@ -1,5 +1,7 @@
 package exception
 
+import "encoding/json"
+
 // Error is a string wrapper that implements `error`.
 // Use this to implement constant exception causes.
 type Error string
@@ -7,4 +9,9 @@ type Error string
 // Error implements `error`.
 func (e Error) Error() string {
 	return string(e)
+}
+
+// MarshalJSON implements json.Marshaler.
+func (e Error) MarshalJSON() ([]byte, error) {
+	return json.Marshal(string(e))
 }
