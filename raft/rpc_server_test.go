cb80b88d9a87a2dc03cf19e23fc2529b7b836ded
Will Charczuk <will@blendlabs.com>
2018-07-03 12:46:31 -0400

    updates

--- raft/rpc_server_test.go
+++ raft/rpc_server_test.go
@@ -3,6 +3,7 @@ package raft
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -16,6 +17,25 @@ func TestNewRPCServer(t *testing.T) {
 	assert.Nil(s.Logger())
 	assert.Equal(DefaultBindAddr, s.BindAddr())
 	assert.Equal(DefaultServerTimeout, s.Timeout())
+}
+
+func TestRPCServerDecode(t *testing.T) {
+	assert := assert.New(t)
+
+	s := NewRPCServer()
+
+	assert.NotNil(s.decode(nil, &http.Request{}))
+
+	output := bytes.NewBuffer([]byte(`{
+	"foo":"bar",
+	"buzz":"jazz"
+}`))
+	req := &http.Request{Body: ioutil.NopCloser(output)}
+	verify := make(map[string]interface{})
+	assert.Nil(s.decode(&verify, req))
+
+	assert.Equal("bar", verify["foo"])
+	assert.Equal("jazz", verify["buzz"])
 }
 
 func TestRPCServerEncode(t *testing.T) {
