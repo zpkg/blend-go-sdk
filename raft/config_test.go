8b61082c3cb490552f75c39678ae412a3c4837fe
Will Charczuk <will@blendlabs.com>
2018-07-11 17:38:52 -0700

    didnt get it done

--- raft/config_test.go
+++ raft/config_test.go
@@ -19,34 +19,3 @@ func TestConfig(t *testing.T) {
 
 	assert.Equal(time.Second, (&Config{ElectionTimeout: time.Second}).GetElectionTimeout())
 }
-
-func TestConfigGetPeersFiltered(t *testing.T) {
-	assert := assert.New(t)
-
-	unfiltered := (&Config{
-		Peers: []string{
-			"worker-0",
-			"worker-1",
-			"worker-2",
-			"worker-3",
-			"worker-4",
-		},
-	}).GetPeersFiltered()
-	assert.Len(unfiltered, 5)
-
-	filtered := (&Config{
-		ExcludePeer: "worker-2",
-		Peers: []string{
-			"worker-0",
-			"worker-1",
-			"worker-2",
-			"worker-3",
-			"worker-4",
-		},
-	}).GetPeersFiltered()
-
-	assert.Len(filtered, 4)
-	assert.None(filtered, func(v interface{}) bool {
-		return v.(string) == "worker-2"
-	})
-}
