139b4817931c366001046947430239211197c7e4
Will Charczuk <will@blendlabs.com>
2018-08-13 18:18:17 -0700

    typo.

--- db/package.go
+++ db/package.go
@@ -1,4 +1,4 @@
-// Package db providers a basic abstraction layer above normal database/sql that makes it easier to
+// Package db provides a basic abstraction layer above normal database/sql that makes it easier to
 // interact with the database and organize database related code. It is not intended to replace actual sql
 // (you write queries yourself in sql). It also includes some helpers to organize creating a connection
 // to a database from a config file or object.
