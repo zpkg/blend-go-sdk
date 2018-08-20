af045a44d6f52149f6cb39a1fd73f95e3d3021c5
Will Charczuk <will@blendlabs.com>
2018-08-20 16:04:24 -0700

    name is required

--- cron/job.go
+++ cron/job.go
@@ -2,7 +2,6 @@ package cron
 
 import (
 	"context"
-	"time"
 )
 
 // Job is an interface structs can satisfy to be loaded into the JobManager.
@@ -35,101 +34,4 @@ type EventShouldWriteOutputProvider interface {
 // EnabledProvider is an optional interface that will allow jobs to control if they're enabled.
 type EnabledProvider interface {
 	Enabled() bool
-}
-
-// NewJob returns a new job factory.
-func NewJob() *JobFactory {
-	return &JobFactory{
-		schedule: OnDemand(),
-	}
-}
-
-// JobFactory allows for job creation w/o a fully formed struct.
-type JobFactory struct {
-	name                 string
-	schedule             Schedule
-	timeout              time.Duration
-	action               TaskAction
-	isEnabledProvider    func() bool
-	showMessagesProvider func() bool
-}
-
-// Name returns the job name.
-func (jf *JobFactory) Name() string {
-	return jf.name
-}
-
-// WithName sets the job name.
-func (jf *JobFactory) WithName(name string) *JobFactory {
-	jf.name = name
-	return jf
-}
-
-// Schedule returns the job schedule.
-func (jf *JobFactory) Schedule() Schedule {
-	return jf.schedule
-}
-
-// WithSchedule sets the schedule for the job.
-func (jf *JobFactory) WithSchedule(schedule Schedule) *JobFactory {
-	jf.schedule = schedule
-	return jf
-}
-
-// Timeout returns the job timeout.
-func (jf *JobFactory) Timeout() time.Duration {
-	return jf.timeout
-}
-
-// WithTimeout sets the timeout.
-func (jf *JobFactory) WithTimeout(timeout time.Duration) *JobFactory {
-	jf.timeout = timeout
-	return jf
-}
-
-// Execute runs the job action if it's set.
-func (jf *JobFactory) Execute(ctx context.Context) error {
-	if jf.action != nil {
-		return jf.action(ctx)
-	}
-	return nil
-}
-
-// WithAction sets the job action.
-func (jf *JobFactory) WithAction(action TaskAction) *JobFactory {
-	jf.action = action
-	return jf
-}
-
-// Action returns the job action.
-func (jf *JobFactory) Action() TaskAction {
-	return jf.action
-}
-
-// WithIsEnabledProvider sets the enabled provider for the job.
-func (jf *JobFactory) WithIsEnabledProvider(provider func() bool) *JobFactory {
-	jf.isEnabledProvider = provider
-	return jf
-}
-
-// IsEnabled returns if the job is enabled.
-func (jf *JobFactory) IsEnabled() bool {
-	if jf.isEnabledProvider != nil {
-		return jf.isEnabledProvider()
-	}
-	return true
-}
-
-// WithShowMessagesProvider sets the enabled provider for the job.
-func (jf *JobFactory) WithShowMessagesProvider(provider func() bool) *JobFactory {
-	jf.showMessagesProvider = provider
-	return jf
-}
-
-// ShowMessages returns if the job should trigger logging events.
-func (jf *JobFactory) ShowMessages() bool {
-	if jf.showMessagesProvider != nil {
-		return jf.showMessagesProvider()
-	}
-	return true
 }
--- cron/job_test.go
+++ cron/job_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/blend/go-sdk/assert"
 )
@@ -23,21 +22,4 @@ func TestIsJobCanceled(t *testing.T) {
 	}()
 	cancel()
 	wg.Wait()
-}
-
-func TestJobBuilder(t *testing.T) {
-	assert := assert.New(t)
-
-	assert.NotNil(NewJob().Schedule())
-	assert.True(NewJob().IsEnabled())
-	assert.True(NewJob().ShowMessages())
-	assert.Equal("test_job", NewJob().WithName("test_job").Name())
-	assert.Equal(EveryMinute(), NewJob().WithSchedule(EveryMinute()).Schedule())
-	assert.Equal(time.Second, NewJob().WithTimeout(time.Second).Timeout())
-
-	action := TaskAction(func(ctx context.Context) error { return nil })
-	assert.NotNil(NewJob().WithAction(action).Action())
-
-	assert.False(NewJob().WithIsEnabledProvider(func() bool { return false }).IsEnabled())
-	assert.False(NewJob().WithShowMessagesProvider(func() bool { return false }).ShowMessages())
 }
