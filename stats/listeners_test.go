47d6fa34f7eb3b1916d612ef22e343b08d86f034
Will Charczuk <will@blendlabs.com>
2018-09-05 12:33:23 -0700

    listener tweaks

--- stats/listeners_test.go
+++ stats/listeners_test.go
@@ -12,7 +12,31 @@ func TestAddWebListeners(t *testing.T) {
 
 	log := logger.None()
 	AddWebListeners(nil, nil)
-	assert.False(log.HasListener(logger.HTTPResponse, "stats"))
+	assert.False(log.HasListener(logger.HTTPResponse, ListenerNameStats))
 	AddWebListeners(log, NewMockCollector())
-	assert.True(log.HasListener(logger.HTTPResponse, "stats"))
+	assert.True(log.HasListener(logger.HTTPResponse, ListenerNameStats))
+}
+
+func TestAddQueryListeners(t *testing.T) {
+	assert := assert.New(t)
+
+	log := logger.None()
+	AddQueryListeners(nil, nil)
+	assert.False(log.HasListener(logger.Query, ListenerNameStats))
+	AddQueryListeners(log, NewMockCollector())
+	assert.True(log.HasListener(logger.Query, ListenerNameStats))
+}
+
+func TestAddErrorListeners(t *testing.T) {
+	assert := assert.New(t)
+
+	log := logger.None()
+	AddErrorListeners(nil, nil)
+	assert.False(log.HasListener(logger.Warning, ListenerNameStats))
+	assert.False(log.HasListener(logger.Error, ListenerNameStats))
+	assert.False(log.HasListener(logger.Fatal, ListenerNameStats))
+	AddErrorListeners(log, NewMockCollector())
+	assert.True(log.HasListener(logger.Warning, ListenerNameStats))
+	assert.True(log.HasListener(logger.Error, ListenerNameStats))
+	assert.True(log.HasListener(logger.Fatal, ListenerNameStats))
 }
