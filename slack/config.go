665b9055690fb460b46f724dfd3a01c7194bfbe4
Will Charczuk <will@blendlabs.com>
2018-09-06 08:17:17 -0700

    listener tweaks, webhook sender tests

--- slack/config.go
+++ slack/config.go
@@ -4,15 +4,39 @@ import "github.com/blend/go-sdk/util"
 
 // Config represents the required fields for the config.
 type Config struct {
-	WebhookURL string `json:"webhookURL,omitempty" yaml:"webhookURL,omitempty"`
+	Username  string `json:"username,omitempty" yaml:"username,omitempty"`
+	Channel   string `json:"channel,omitempty" yaml:"channel,omitempty"`
+	IconURL   string `json:"iconURL,omitempty" yaml:"iconURL,omitempty"`
+	IconEmoji string `json:"iconEmoji,omitempty" yaml:"iconEmoji,omitempty"`
+	Webhook   string `json:"webhook,omitempty" yaml:"webhook,omitempty"`
 }
 
 // IsZero returns if the config is set or not.
 func (c Config) IsZero() bool {
-	return len(c.WebhookURL) == 0
+	return len(c.Webhook) == 0
 }
 
-// GetWebhookURL returns the webhook url.
-func (c Config) GetWebhookURL(defaults ...string) string {
-	return util.Coalesce.String(c.WebhookURL, "", defaults...)
+// GetUsername returns a property or a default.
+func (c Config) GetUsername(inherited ...string) string {
+	return util.Coalesce.String(c.Username, "", inherited...)
+}
+
+// GetChannel returns a property or default.
+func (c Config) GetChannel(inherited ...string) string {
+	return util.Coalesce.String(c.Channel, "", inherited...)
+}
+
+// GetIconURL returns a property or default.
+func (c Config) GetIconURL(inherited ...string) string {
+	return util.Coalesce.String(c.IconURL, "", inherited...)
+}
+
+// GetIconEmoji returns a property or default.
+func (c Config) GetIconEmoji(inherited ...string) string {
+	return util.Coalesce.String(c.IconEmoji, "", inherited...)
+}
+
+// GetWebhook returns the webhook url.
+func (c Config) GetWebhook(defaults ...string) string {
+	return util.Coalesce.String(c.Webhook, "", defaults...)
 }
