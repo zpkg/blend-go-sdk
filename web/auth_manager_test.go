a31f4f7367d157d4c0daeced07264a21b07b8c12
Will Charczuk <will@blendlabs.com>
2018-09-08 11:27:29 -0700

    test entrypoints for examples

--- web/auth_manager_test.go
+++ web/auth_manager_test.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/blend/go-sdk/assert"
 	"github.com/blend/go-sdk/util"
+	"github.com/blend/go-sdk/uuid"
 )
 
 func TestNewJWTAuthManager(t *testing.T) {
@@ -60,7 +62,44 @@ func TestAuthManagerLogin(t *testing.T) {
 	assert.True(strings.HasPrefix(cookie, fmt.Sprintf("%s=%s", am.CookieName(), session.SessionID)))
 }
 
-func TestAuthManagerVerifySession(t *testing.T) {
+func TestAuthManagerVerifySessionParsed(t *testing.T) {
+	assert := assert.New(t)
+
+	am := NewLocalAuthManager()
+
+	var calledParseHandler bool
+	am.WithParseSessionValueHandler(func(ctx context.Context, sessionID string, state State) (*Session, error) {
+		calledParseHandler = true
+		return &Session{UserID: uuid.V4().String(), SessionID: sessionID}, nil
+	})
+
+	var calledFetchHandler bool
+	am.WithFetchHandler(func(ctx context.Context, sessionID string, state State) (*Session, error) {
+		calledFetchHandler = true
+		return nil, nil
+	})
+
+	var calledValidateHandler bool
+	am.WithValidateHandler(func(ctx context.Context, session *Session, state State) error {
+		calledValidateHandler = true
+		return nil
+	})
+
+	assert.NotNil(am.ParseSessionValueHandler())
+	assert.NotNil(am.FetchHandler())
+	assert.NotNil(am.ValidateHandler())
+
+	r := NewCtx(NewMockResponseWriter(new(bytes.Buffer)), NewMockRequestWithCookie("GET", "/", am.CookieName(), NewSessionID()), nil, nil)
+	session, err := am.VerifySession(r)
+	assert.Nil(err)
+	assert.NotNil(session)
+	assert.True(session.ExpiresUTC.IsZero())
+	assert.True(calledParseHandler)
+	assert.False(calledFetchHandler)
+	assert.True(calledValidateHandler)
+}
+
+func TestAuthManagerVerifySessionFetched(t *testing.T) {
 	assert := assert.New(t)
 
 	am := NewLocalAuthManager()
@@ -90,6 +129,7 @@ func TestAuthManagerVerifySession(t *testing.T) {
 	assert.Nil(err)
 	assert.NotNil(session)
 	assert.False(calledFetchHandler)
+	assert.False(calledValidateHandler)
 
 	r = NewCtx(NewMockResponseWriter(new(bytes.Buffer)), NewMockRequestWithCookie("GET", "/", am.CookieName(), session.SessionID), nil, nil)
 	session, err = am.VerifySession(r)
@@ -115,20 +155,30 @@ func TestAuthManagerVerifySessionExpired(t *testing.T) {
 	assert := assert.New(t)
 
 	am := NewLocalAuthManager()
+	am.WithSessionTimeoutProvider(nil)
+	am.WithParseSessionValueHandler(func(ctx context.Context, sessionID string, state State) (*Session, error) {
+		return &Session{UserID: uuid.V4().String(), SessionID: sessionID, ExpiresUTC: time.Now().UTC().Add(-time.Hour)}, nil
+	})
 
-	r := NewCtx(NewMockResponseWriter(new(bytes.Buffer)), NewMockRequest("GET", "/"), nil, nil)
+	var calledValidateHandler bool
+	am.WithValidateHandler(func(ctx context.Context, session *Session, state State) error {
+		calledValidateHandler = true
+		return nil
+	})
 
+	res := NewMockResponseWriter(new(bytes.Buffer))
+	r := NewCtx(res, NewMockRequestWithCookie("GET", "/", am.CookieName(), NewSessionID()), nil, nil)
 	session, err := am.VerifySession(r)
 	assert.Nil(err)
 	assert.Nil(session)
+	assert.False(calledValidateHandler)
 
-	r = NewCtx(NewMockResponseWriter(new(bytes.Buffer)), NewMockRequest("GET", "/"), nil, nil)
-	session, err = am.Login("bailey@blend.com", r)
-	assert.Nil(err)
-	assert.NotNil(session)
+	// assert the cookie is expired ...
+	cookies := ReadSetCookies(res.Header())
+	assert.NotEmpty(cookies)
 
-	r = NewCtx(NewMockResponseWriter(new(bytes.Buffer)), NewMockRequestWithCookie("GET", "/", am.CookieName(), session.SessionID), nil, nil)
-	session, err = am.VerifySession(r)
-	assert.Nil(err)
-	assert.NotNil(session)
+	cookie := cookies[0]
+	assert.Equal(am.CookieName(), cookie.Name)
+	assert.Equal(am.CookiePath(), cookie.Path)
+	assert.True(cookie.Expires.Before(time.Now().UTC()), "the cookie should be expired")
 }
