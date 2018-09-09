ecf27edb00a93d9b8ee8d639c0c2ed930e269652
Will Charczuk <will@blendlabs.com>
2018-09-09 10:21:00 -0700

    caught an issue w/ internal error for views

--- web/view_cache_test.go
+++ web/view_cache_test.go
@@ -2,9 +2,12 @@ package web
 
 import (
 	"bytes"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/blend/go-sdk/assert"
+	"github.com/blend/go-sdk/exception"
 )
 
 func TestViewCacheAddRawViews(t *testing.T) {
@@ -69,4 +72,86 @@ func TestViewCacheCachingDisabled(t *testing.T) {
 	buf = bytes.NewBuffer(nil)
 	assert.Nil(tmp.Execute(buf, nil))
 	assert.Equal("baz", buf.String())
+}
+
+func TestViewCacheInternalError(t *testing.T) {
+	assert := assert.New(t)
+
+	vc := NewViewCache()
+	assert.Nil(vc.Initialize())
+
+	ler, _ := vc.InternalError(exception.Class("only a test")).(*loggedErrorResult)
+	assert.NotNil(ler)
+	vr := ler.Result.(*ViewResult)
+	assert.Equal(vc.InternalErrorTemplateName(), vr.ViewName)
+	assert.Equal(http.StatusInternalServerError, vr.StatusCode)
+	assert.NotNil(vr.Views)
+	assert.NotNil(vr.ViewModel, fmt.Sprintf("%#v\n", vr))
+}
+
+func TestViewCacheNotFound(t *testing.T) {
+	assert := assert.New(t)
+
+	vc := NewViewCache()
+	assert.Nil(vc.Initialize())
+
+	vr, _ := vc.NotFound().(*ViewResult)
+	assert.NotNil(vr)
+	assert.Equal(vc.NotFoundTemplateName(), vr.ViewName)
+	assert.Equal(http.StatusNotFound, vr.StatusCode)
+	assert.NotNil(vr.Views)
+	assert.Nil(vr.ViewModel)
+}
+
+func TestViewCacheNotAuthorized(t *testing.T) {
+	assert := assert.New(t)
+
+	vc := NewViewCache()
+	assert.Nil(vc.Initialize())
+
+	vr, _ := vc.NotAuthorized().(*ViewResult)
+	assert.NotNil(vr)
+	assert.Equal(vc.NotAuthorizedTemplateName(), vr.ViewName)
+	assert.Equal(http.StatusForbidden, vr.StatusCode)
+	assert.NotNil(vr.Views)
+	assert.Nil(vr.ViewModel)
+}
+
+func TestViewCacheView(t *testing.T) {
+	assert := assert.New(t)
+
+	vc := NewViewCache()
+	vc.AddLiterals(
+		`{{ define "test" }}{{.ViewModel}}{{end}}`,
+	)
+	assert.Nil(vc.Initialize())
+
+	vr, _ := vc.View("test", "foo").(*ViewResult)
+	assert.NotNil(vr)
+	assert.Equal("test", vr.ViewName)
+	assert.Equal(http.StatusOK, vr.StatusCode)
+	assert.Equal("foo", vr.ViewModel)
+	assert.NotNil(vr.Views)
+
+	// handle if the view is not found ...
+	ler, _ := vc.View("not-test", "foo").(*loggedErrorResult)
+	assert.NotNil(ler)
+	vr, _ = ler.Result.(*ViewResult)
+	assert.Equal(vc.InternalErrorTemplateName(), vr.ViewName)
+	assert.Equal(http.StatusInternalServerError, vr.StatusCode)
+	assert.NotNil(vr.Views)
+}
+
+func TestViewCacheViewError(t *testing.T) {
+	assert := assert.New(t)
+
+	vc := NewViewCache()
+
+	vcr := vc.viewError(exception.Class("test error")).(*ViewResult)
+	assert.NotNil(vcr)
+	assert.Equal(DefaultTemplateNameInternalError, vcr.ViewName)
+	assert.Equal(http.StatusInternalServerError, vcr.StatusCode)
+	assert.Equal(exception.Class("test error"), vcr.ViewModel)
+	assert.NotNil(vcr.Template)
+	assert.NotNil(vcr.Views)
 }
