686fbd53141bd0bbee60d07714db0513ee9abfbf
Will Charczuk <will@blendlabs.com>
2018-09-09 12:12:42 -0700

    timeout test

---
+++ web/timeout_test.go
@@ -0,0 +1,42 @@
+package web
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestTimeout(t *testing.T) {
+	assert := assert.New(t)
+
+	app := New().
+		WithBindAddr("127.0.0.1:0").
+		WithDefaultMiddleware(WithTimeout(1 * time.Millisecond))
+
+	var didFinish bool
+	app.GET("/long", func(_ *Ctx) Result {
+		time.Sleep(2 * time.Millisecond)
+		didFinish = true
+		return NoContent
+	})
+	app.GET("/short", func(_ *Ctx) Result {
+		didFinish = true
+		return NoContent
+	})
+
+	go func() {
+		app.Start()
+	}()
+	defer app.Shutdown()
+	<-app.NotifyStarted()
+
+	_, err := http.Get("http://" + app.Listener().Addr().String() + "/long")
+	assert.Nil(err)
+	assert.False(didFinish)
+
+	_, err = http.Get("http://" + app.Listener().Addr().String() + "/short")
+	assert.Nil(err)
+	assert.True(didFinish)
+}
