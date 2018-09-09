1d5a6ddb4322cc6c00c8ee67c5aea616e1763e77
Will Charczuk <will@blendlabs.com>
2018-09-08 17:57:07 -0700

    coverage

--- web/view_funcs.go
+++ web/view_funcs.go
@@ -33,19 +33,19 @@ func ViewFuncs() template.FuncMap {
 			return fmt.Sprintf("$%0.2f", d)
 		},
 		"duration": func(d time.Duration) string {
-			if d > time.Hour {
+			if d >= time.Hour {
 				return fmt.Sprintf("%0.2fh", float64(d)/float64(time.Hour))
 			}
-			if d > time.Minute {
+			if d >= time.Minute {
 				return fmt.Sprintf("%0.2fm", float64(d)/float64(time.Minute))
 			}
-			if d > time.Second {
+			if d >= time.Second {
 				return fmt.Sprintf("%0.2fs", float64(d)/float64(time.Second))
 			}
-			if d > time.Millisecond {
+			if d >= time.Millisecond {
 				return fmt.Sprintf("%0.2fms", float64(d)/float64(time.Millisecond))
 			}
-			if d > time.Microsecond {
+			if d >= time.Microsecond {
 				return fmt.Sprintf("%0.2fµs", float64(d)/float64(time.Microsecond))
 			}
 			return fmt.Sprintf("%dns", d)
