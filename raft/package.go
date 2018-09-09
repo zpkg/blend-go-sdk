145ba1f7cb9aa4eee70f90ee72c03db42bc9d9c2
Will Charczuk <will@blendlabs.com>
2018-09-08 17:00:25 -0700

    removing raft

--- raft/package.go
+++
@@ -1,4 +0,0 @@
-// Package raft is a very spare implementation of the raft consensus protocol concentrating on leader elections.
-// It uses HTTP for rpc, and is designed to be used in an unreliable network environment.
-// It is *extremely* experimental at the moment, and not designed for production use.
-package raft
