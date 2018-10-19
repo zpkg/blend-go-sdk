8d47c3cd10bf197a6327bfb8fb12179a971a6d2a
Will Charczuk <will@blendlabs.com>
2018-10-18 17:04:27 -0700

    adds no content example

---
+++ web/_examples/no_content/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/blend/go-sdk/graceful"
+	"github.com/blend/go-sdk/logger"
+	"github.com/blend/go-sdk/web"
+)
+
+func main() {
+	app := web.New().WithLogger(logger.All())
+
+	app.GET("/204", func(_ *web.Ctx) web.Result {
+		return web.NoContent
+	})
+	app.GET("/500", func(_ *web.Ctx) web.Result {
+		return web.JSON.InternalError(fmt.Errorf("this is only a test"))
+	})
+
+	if err := graceful.Shutdown(app); err != nil {
+		app.Logger().SyncFatalExit(err)
+	}
+}
