0bb10bb44645b37fd47e58c0d4509e026446b347
Will Charczuk <will@blend.com>
2019-02-04 10:15:39 -0800

    WTO

--- cmd/job/main.go
+++ cmd/job/main.go
@@ -7,20 +7,12 @@ import (
 	"os"
 	"strings"
 
-	"github.com/blend/go-sdk/airbrake"
-	"github.com/blend/go-sdk/aws"
-	"github.com/blend/go-sdk/aws/ses"
 	"github.com/blend/go-sdk/configutil"
 	"github.com/blend/go-sdk/cron"
-	"github.com/blend/go-sdk/datadog"
-	"github.com/blend/go-sdk/diagnostics"
-	"github.com/blend/go-sdk/email"
 	"github.com/blend/go-sdk/graceful"
 	"github.com/blend/go-sdk/jobkit"
 	"github.com/blend/go-sdk/logger"
 	"github.com/blend/go-sdk/sh"
-	"github.com/blend/go-sdk/slack"
-	"github.com/blend/go-sdk/stats"
 	"github.com/blend/go-sdk/stringutil"
 )
 
@@ -31,46 +23,22 @@ var schedule = flag.String("schedule", "*/1 * * * * * *", "The job schedule as a
 var configPath = flag.String("config", "config.yml", "The job config path")
 var timeout = flag.Duration("timeout", 0, "The timeout")
 
+type jobConfig struct {
+	jobkit.Config
+	jobkit.JobConfig
+}
+
 func main() {
+	var err error
 	flag.Parse()
 
-	schedule, err := cron.ParseString(*schedule)
-	if err != nil {
-		logger.FatalExit(err)
-	}
-
-	var config jobkit.Config
+	var config jobConfig
 	if err := configutil.Read(&config, *configPath); !configutil.IsIgnored(err) {
 		logger.FatalExit(err)
 	}
 
 	log := logger.NewFromConfig(&config.Logger)
 	log.WithEnabled(cron.FlagStarted, cron.FlagComplete, cron.FlagFixed, cron.FlagBroken, cron.FlagFailed, cron.FlagCancelled)
-
-	// set up myriad of notification targets
-	var emailClient email.Sender
-	if !config.AWS.IsZero() {
-		log.SyncInfof("email notifications: %s", logger.ColorBlue.Apply("enabled"))
-		emailClient = ses.New(aws.MustNewSession(&config.AWS))
-	}
-	var slackClient slack.Sender
-	if !config.Slack.IsZero() {
-		log.SyncInfof("slack notifications: %s", logger.ColorBlue.Apply("enabled"))
-		slackClient = slack.New(&config.Slack)
-	}
-	var statsClient stats.Collector
-	if !config.Datadog.IsZero() {
-		log.SyncInfof("datadog: %s", logger.ColorBlue.Apply("enabled"))
-		statsClient, err = datadog.NewCollector(&config.Datadog)
-		if err != nil {
-			logger.FatalExit(err)
-		}
-	}
-	var errorClient diagnostics.Notifier
-	if !config.Airbrake.IsZero() {
-		log.SyncInfof("airbrake: %s", logger.ColorBlue.Apply("enabled"))
-		errorClient = airbrake.MustNew(&config.Airbrake)
-	}
 
 	var command []string
 	if *exec != "" {
@@ -90,24 +58,22 @@ func main() {
 		return sh.ForkContext(ctx, command[0], args(command...)...)
 	}
 
-	var notificationsConfig *jobkit.NotificationsConfig
-	if cfg, ok := config.Notifications[*name]; ok {
-		notificationsConfig = &cfg
+	schedule, err := cron.ParseString(*schedule)
+	if err != nil {
+		logger.FatalExit(err)
 	}
 
-	jobs := cron.NewFromConfig(&config.Config).WithLogger(log)
-	jobs.LoadJob(jobkit.NewJob(action).
-		WithName(*name).
-		WithSchedule(schedule).
-		WithLogger(log).
-		WithNotificationsConfig(notificationsConfig).
-		WithEmailClient(emailClient).
-		WithStatsClient(statsClient).
-		WithSlackClient(slackClient).
-		WithErrorClient(errorClient))
+	job, err := jobkit.New(&config.JobConfig, &config.Config, action)
+	if err != nil {
+		logger.FatalExit(err)
+	}
+	job.WithLogger(log)
+	job.WithSchedule(schedule)
 
-	ws := jobkit.NewManagementServer(jobs, &config)
-	ws.WithLogger(log)
+	jobs := cron.NewFromConfig(&config.Config.Config).WithLogger(log)
+	jobs.LoadJob(job)
+
+	ws := jobkit.NewManagementServer(jobs, &config.Config).WithLogger(log)
 	if *bind != "" {
 		ws.WithBindAddr(*bind)
 	}
