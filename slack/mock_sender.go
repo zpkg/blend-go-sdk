f35d285456486c62e32fe28efc8169e20e8f8d33
Will Charczuk <will@blend.com>
2019-02-03 20:34:59 -0800

    fixing mock sender

--- slack/mock_sender.go
+++ slack/mock_sender.go
@@ -1,17 +1,51 @@
 package slack
 
-import "context"
+import (
+	"context"
+)
 
 // NewMockWebhookSender creates a new mock sender.
 func NewMockWebhookSender() MockWebhookSender {
 	return MockWebhookSender(make(chan Message))
 }
 
+var (
+	_ Sender = (*MockWebhookSender)(nil)
+)
+
 // MockWebhookSender is a mocked sender.
 type MockWebhookSender chan Message
 
 // Send sends a mocked message.
 func (ms MockWebhookSender) Send(ctx context.Context, m Message) error {
+	ms <- m
+	return nil
+}
+
+// PostMessage sends a mocked message.
+func (ms MockWebhookSender) PostMessage(channel, text string, options ...MessageOption) error {
+	m := Message{
+		Channel: channel,
+		Text:    text,
+	}
+	for _, option := range options {
+		option(&m)
+	}
+
+	ms <- m
+	return nil
+}
+
+// PostMessageContext sends a mocked message.
+func (ms MockWebhookSender) PostMessageContext(ctx context.Context, channel, text string, options ...MessageOption) error {
+	m := Message{
+		Channel: channel,
+		Text:    text,
+	}
+	for _, option := range options {
+		option(&m)
+	}
+
 	ms <- m
 	return nil
 }
