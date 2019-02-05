ded94153dccc3ecb3dcc49d484f2e531ffabb5d0
Will Charczuk <will@blend.com>
2019-02-05 12:01:15 -0800

    adds `ref`

---
+++ ref/ref_test.go
@@ -0,0 +1,32 @@
+package ref
+
+import (
+	"testing"
+	"time"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestRef(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.NotNil(String("foo"))
+	assert.NotEmpty(Strings("foo", "bar"))
+
+	assert.NotNil(Byte('b'))
+	assert.NotNil(Rune('b'))
+
+	assert.NotNil(Uint8(0))
+	assert.NotNil(Uint16(0))
+	assert.NotNil(Uint32(0))
+	assert.NotNil(Uint64(0))
+	assert.NotNil(Int8(0))
+	assert.NotNil(Int16(0))
+	assert.NotNil(Int32(0))
+	assert.NotNil(Int64(0))
+	assert.NotNil(Int(0))
+	assert.NotNil(Float32(0))
+	assert.NotNil(Float64(0))
+	assert.NotNil(Time(time.Time{}))
+	assert.NotNil(Duration(0))
+}
