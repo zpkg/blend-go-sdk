e91a9f93f21e9baf5fc7970e4072dfd45bcb3d44
Will Charczuk <will@blend.com>
2019-03-07 15:01:02 -0800

    adds web views viewmodel $.Wrap to pass viewmodel context down to controls

---
+++ web/view_model_test.go
@@ -0,0 +1,27 @@
+package web
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestViewModelWrap(t *testing.T) {
+	assert := assert.New(t)
+
+	indexTemplate := `{{ define "index" }}{{ range $index, $obj := .ViewModel }}<div>{{ template "control" ( $.Wrap $obj ) }}</div>{{ end }}{{ end }}`
+	controlTemplate := `{{ define "control" }}{{ if .Ctx }}{{ .ViewModel }}{{ end }}{{ end }}`
+
+	app := New()
+	app.Views().AddLiterals(indexTemplate, controlTemplate)
+
+	app.GET("/", func(r *Ctx) Result {
+		return r.Views().View("index", []string{"foo", "bar", "baz"})
+	})
+
+	contents, meta, err := app.Mock().Get("/").BytesWithMeta()
+	assert.Nil(err)
+	assert.Equal(http.StatusOK, meta.StatusCode, string(contents))
+	assert.Equal("<div>foo</div><div>bar</div><div>baz</div>", string(contents))
+}
