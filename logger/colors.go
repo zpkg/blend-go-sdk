b1b72dd0df1c0d07b81feff7bbe19afd43522c30
Will Charczuk <will@blend.com>
2019-03-18 21:30:40 -0700

    updates

---
+++ logger/colors.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/blend/go-sdk/ansi"
+)
+
+var (
+	// DefaultFlagTextColors is the default color for each known flag.
+	DefaultFlagTextColors = map[Flag]ansi.Color{
+		Info:    ansi.ColorLightWhite,
+		Silly:   ansi.ColorLightBlack,
+		Debug:   ansi.ColorLightYellow,
+		Warning: ansi.ColorLightYellow,
+		Error:   ansi.ColorRed,
+		Fatal:   ansi.ColorRed,
+	}
+
+	// DefaultFlagTextColor is the default flag color.
+	DefaultFlagTextColor = ansi.ColorLightWhite
+)
+
+// GetFlagTextColor returns the color for a flag.
+func GetFlagTextColor(flag Flag) ansi.Color {
+	if color, hasColor := DefaultFlagTextColors[flag]; hasColor {
+		return color
+	}
+	return DefaultFlagTextColor
+}
+
+// ColorizeByStatusCode returns a value colored by an http status code.
+func ColorizeByStatusCode(statusCode int, value string) string {
+	if statusCode >= http.StatusOK && statusCode < 300 { //the http 2xx range is ok
+		return ansi.ColorGreen.Apply(value)
+	} else if statusCode == http.StatusInternalServerError {
+		return ansi.ColorRed.Apply(value)
+	}
+	return ansi.ColorYellow.Apply(value)
+}
+
+// ColorizeStatusCode colorizes a status code.
+func ColorizeStatusCode(statusCode int) string {
+	return ColorizeByStatusCode(statusCode, strconv.Itoa(statusCode))
+}
