d6294bca87999b445e6f4a497f4b94e67c367bf2
Alex Fish <alexf@blend.com>
2019-03-25 18:31:41 -0700

    fix.

--- secrets/mock.go
+++ secrets/mock.go
@@ -8,19 +8,19 @@ import (
 	"net/url"
 )
 
-// HTTPClient is a Client that can send http requests.
+// HTTPClient is a client that can send http requests.
 type HTTPClient interface {
 	Do(*http.Request) (*http.Response, error)
 }
 
-// NewMockHTTPClient returns a new mock http Client.
+// NewMockHTTPClient returns a new mock http client.
 func NewMockHTTPClient() *MockHTTPClient {
 	return &MockHTTPClient{
 		contents: make(map[string]*http.Response),
 	}
 }
 
-// MockHTTPClient is a mock http Client.
+// MockHTTPClient is a mock http client.
 type MockHTTPClient struct {
 	contents map[string]*http.Response
 }
