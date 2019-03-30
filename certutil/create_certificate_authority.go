bca10b4e4a6083e52d1d8da20149362d2ad06105
Will Charczuk <will@blend.com>
2019-03-29 22:13:18 -0700

    add sucks

---
+++ certutil/create_certificate_authority.go
@@ -0,0 +1,33 @@
+package certutil
+
+import (
+	"crypto/rand"
+	"crypto/x509"
+
+	"github.com/blend/go-sdk/exception"
+)
+
+// CreateCertificateAuthority creates a ca cert bundle from a given set of options.
+// The cert bundle can be used to generate client and server certificates.
+func CreateCertificateAuthority(options ...CertOption) (*CertBundle, error) {
+	createOptions := DefaultOptionsCertificateAuthority
+
+	if err := ResolveCertOptions(&createOptions, options...); err != nil {
+		return nil, nil
+	}
+
+	var output CertBundle
+	output.PrivateKey = createOptions.PrivateKey
+	output.PublicKey = &createOptions.PrivateKey.PublicKey
+	der, err := x509.CreateCertificate(rand.Reader, &createOptions.Certificate, &createOptions.Certificate, output.PublicKey, output.PrivateKey)
+	if err != nil {
+		return nil, exception.New(err)
+	}
+	cert, err := x509.ParseCertificate(der)
+	if err != nil {
+		return nil, exception.New(err)
+	}
+	output.CertificateDERs = [][]byte{der}
+	output.Certificates = []x509.Certificate{*cert}
+	return &output, nil
+}
---
+++ certutil/create_certificate_authority_test.go
@@ -0,0 +1,18 @@
+package certutil
+
+import (
+	"testing"
+
+	"github.com/blend/go-sdk/assert"
+)
+
+func TestCreateCA(t *testing.T) {
+	assert := assert.New(t)
+
+	ca, err := CreateCertificateAuthority()
+	assert.Nil(err)
+	assert.NotNil(ca.PrivateKey)
+	assert.NotNil(ca.PublicKey)
+	assert.Len(ca.Certificates, 1)
+	assert.Len(ca.CertificateDERs, 1)
+}
