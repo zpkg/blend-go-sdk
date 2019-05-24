317b588999d00f3246506a09c183c96bb5d5bf23
mike.spolarich <mike.spolarich@blend.com>
2019-05-24 13:54:29 -0700

    Cleanup the singleton

--- db/default.go
+++ db/default.go
@@ -1,30 +1,23 @@
 package db
 
-import (
-	"sync"
-)
-
 var (
 	defaultConnection *Connection
-	defaultLock       = sync.Mutex{}
 )
 
 // SetDefault sets an alias created with `CreateDbAlias` as default. This lets you refer to it later via. `Default()`
 //
-//	spiffy.CreateDbAlias("main", spiffy.NewDbConnection("localhost", "test_db", "", ""))
-//	spiffy.SetDefault("main")
-//	execErr := spiffy.Default().Execute("select 'ok!')
+//	db.CreateDbAlias("main", spiffy.NewDbConnection("localhost", "test_db", "", ""))
+//	db.SetDefault("main")
+//	execErr := db.Default().Execute("select 'ok!')
 //
 // This will then let you refer to the alias via. `Default()`
 func SetDefault(conn *Connection) {
-	defaultLock.Lock()
 	defaultConnection = conn
-	defaultLock.Unlock()
 }
 
 // Default returns a reference to the DbConnection set as default.
 //
-//	spiffy.Default().Exec("select 'ok!")
+//	db.Default().Exec("select 'ok!")
 //
 func Default() *Connection {
 	return defaultConnection
